d0ea8de473112c5980266cbc402ecb28c550b4fc
Sazzad Hossain <eresh24@gmail.com>
2025-07-22 00:53:04 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log(`Server running on http://localhost:3000`);
+setInterval(() => {' > index.js && \
+    echo '  console.log("Hello, this message appears every 5 seconds!");' >> index.js && \
+    echo '}, 5000);
