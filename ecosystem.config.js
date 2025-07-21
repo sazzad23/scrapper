27e0878b3d49d395d932fe51397e2c9b345d4a17
Sazzad Hossain <eresh24@gmail.com>
2025-07-21 22:14:40 +0600

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+export default {
+  apps: [
+    {
+      name: 'puppeteer-app',
+      script: './index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      }
+    }
+  ]
+};
